d29029f59966293ba8c7fc65d52ac884488b9b3c
Stephan Saguier <100237800+SSC023@users.noreply.github.com>
2022-05-11 16:36:33 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Vestido',
+            category: 'Vestidos',
+            image: '/images/product-1.jpg',
+            price: '50.000 GS',
+            brand: 'Albertina',
+            description: 'Vestido Small de albertina',
+        },
+        {
+            _id: '2',
+            name: 'Jean',
+            category: 'Pantalon',
+            image: '/images/product-1.jpg',
+            price: '70.000 GS',
+            brand: 'Zara',
+            description: 'Jean Medium de albertina',
+        },
+    ],
+};
+export default data;
